e5da98d566475441451058ccacf07e58d9eeeb8e
Dong Hyun Kim <w1057216@gmail.com>
2018-03-04 22:53:44 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 // const PORT = 3000;
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
